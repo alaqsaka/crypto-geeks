ffe60095bc47aca1c8e66d2d0a6af21af9963ecb
alaqsaka <aqsaabidin15@gmail.com>
2021-11-06 23:01:36 +0700

    chore: finishing layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./App.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import "antd/dist/antd.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);

b3427324a816c907d9784300e6db0e7a18fc1ea3
alaqsaka <aqsaabidin15@gmail.com>
2021-11-13 19:26:00 +0700

    chore: adding loader.jsx

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,6 +15,7 @@ import {
     ThunderboltOutlined } from '@ant-design/icons';
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
 import LineChart from './LineChart'
+import Loader from './Loader'
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -26,7 +27,7 @@ const CryptoDetails = () => {
     const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timePeriod });
     const cryptoDetails = data?.data?.coin;
     
-    if (isFetching) return 'Loading..';
+    if (isFetching) return <Loader />;
 
     console.log(data)
 
